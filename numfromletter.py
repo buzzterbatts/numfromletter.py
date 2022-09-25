a34af63afd215abcfd52b6877d2e1b198df575ac
daveglacy <96998955+daveglacy@users.noreply.github.com>
2022-09-25 01:05:14 -0600

    Create numfromletter.py

---
+++ numfromletter.py
@@ -0,0 +1,54 @@
+def numfromletter(let):
+    if let == "a":
+        return 27
+    if let == "b":
+        return 2
+    if let == "c":
+        return 3
+    if let == "d":
+        return 4
+    if let == "e":
+        return 5
+    if let == "f":
+        return 6
+    if let == "g":
+        return 7
+    if let == "h":
+        return 8
+    if let == "i":
+        return 9
+    if let == "j":
+        return 10
+    if let == "k":
+        return 11
+    if let == "l":
+        return 12
+    if let == "m":
+        return 13
+    if let == "n":
+        return 14
+    if let == "o":
+        return 15
+    if let == "p":
+        return 16
+    if let == "q":
+        return 17
+    if let == "r":
+        return 18
+    if let == "s":
+        return 19
+    if let == "t":
+        return 20
+    if let == "u":
+        return 21
+    if let == "v":
+        return 22
+    if let == "w":
+        return 23
+    if let == "x":
+        return 24
+    if let == "y":
+        return 25
+    if let == "z":
+        return 26
+    
