3eba51d5e9385d9ff577fa8bf6ab9c6dc3716641
daveglacy <96998955+daveglacy@users.noreply.github.com>
2022-09-26 01:48:46 -0600

    Create brain.py

---
+++ brain.py
@@ -0,0 +1,22 @@
+"
+brain1 =[
+    ["hi", "hello"],
+    ["website","10.0.0.67"],
+    ["who are you", "tom"],
+    ["what do you do", "not much"],
+    ["who made you", "aidyn"],
+    ["whats up", "the sky"],
+    ["im bored", "me to"],
+    ["turn off", "goodbye"],
+    ["hows it going","good hbu"],
+    ["good","What do you need"],
+    ["to do", to_do1],
+    ["whats the point", "what matters to you"],
+    ["tell a joke","knock knock"],
+    ["whos there", "tank"],
+    ["tank who", "thank you"],
+    ["cool", "leave me alone"],
+    ["thank you", "go away"],
+    ["are you alive", "no thankfully"],
+    ["bihjuh","nul"]#why will this return none in terminal but work as brain1[12][1]?
+]
